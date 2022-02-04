45421841b929d66a641325651747fa555d51ea91
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-02-04 16:09:25 +0100

    Created command in Player.java to move the player forward, with collision check towards the field's boundaries

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -23,20 +23,31 @@ public class Player {
 
         if(dir == 0) {
             dir++;
+            System.out.println("You turn to the right...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 1) {
             dir++;
+            System.out.println("You turn to the right...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 2) {
             dir++;
+            System.out.println("You turn to the right...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 3) {
             dir = 0;
+            System.out.println("You turn to the right...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else {
-            System.out.println("ERROR: Turn right critical error...");
+            System.out.println("ERROR: Turn right error...");
         }
-
-        System.out.println("You turn to the right...");
-        System.out.println();
-        field.getField()[posRow][posColumn] = playerChar[dir];
-        field.printField();
 
     }
 
@@ -45,20 +56,72 @@ public class Player {
 
         if(dir == 0) {
             dir = 3;
+            System.out.println("You turn to the left...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 3) {
             dir--;
+            System.out.println("You turn to the left...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 2) {
             dir--;
+            System.out.println("You turn to the left...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else if(dir == 1) {
             dir--;
+            System.out.println("You turn to the left...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
         } else {
-            System.out.println("ERROR: Turn left critical error...");
+            System.out.println("ERROR: Turn left error...");
         }
 
-        System.out.println("You turn to the left...");
-        System.out.println();
-        field.getField()[posRow][posColumn] = playerChar[dir];
-        field.printField();
+    }
+
+    //command to move the player forward
+    public void moveForward(Field field) {
+
+        if(dir == 0 && field.getField()[posRow - 1][posColumn] != '#') {
+            field.getField()[posRow][posColumn] = ' ';
+            field.getField()[posRow - 1][posColumn] = playerChar[dir];
+            posRow--;
+            System.out.println("You move forward...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
+        } else if(dir == 1 && field.getField()[posRow][posColumn + 1] != '#') {
+            field.getField()[posRow][posColumn] = ' ';
+            field.getField()[posRow][posColumn + 1] = playerChar[dir];
+            posColumn++;
+            System.out.println("You move forward...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
+        } else if(dir == 2 && field.getField()[posRow + 1][posColumn] != '#') {
+            field.getField()[posRow][posColumn] = ' ';
+            field.getField()[posRow + 1][posColumn] = playerChar[dir];
+            posRow++;
+            System.out.println("You move forward...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
+        } else if(dir == 3 && field.getField()[posRow][posColumn - 1] != '#') {
+            field.getField()[posRow][posColumn] = ' ';
+            field.getField()[posRow][posColumn - 1] = playerChar[dir];
+            posColumn--;
+            System.out.println("You move forward...");
+            System.out.println();
+            field.getField()[posRow][posColumn] = playerChar[dir];
+            field.printField();
+        } else {
+            System.out.println("There is a wall in front of you...");
+        }
 
     }
 
